bb349d654aff51c6a64b831ae541197f6cee154c
Chase Johnson <chaseaj@amazon.com>
2017-04-23 18:33:55 +0100

    Naive Bayes implementation on text for email origin determination

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -14,7 +14,9 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+### import the sklearn module for GaussianNB
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -22,12 +24,19 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
-
 #########################################################
-### your code goes here ###
+ ### create classifier
+clf = GaussianNB()
 
+### fit the classifier on the training features and labels
+clf.fit(features_train, labels_train)
 
+### use the trained classifier to predict labels for the test features
+pred = clf.predict(features_test)
+
+### Print the accuracy
+### accuracy = no. of points classified correctly / all points (in test set)
+print accuracy_score(pred, labels_test)
 #########################################################
 
 
